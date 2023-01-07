d3b73bab87711825f4e1d437a55d36f7a13ab630
Christopher David <chris@arcade.city>
2023-01-07 11:45:45 -0600

    initial nostr-tools test

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {generatePrivateKey, getPublicKey} from 'nostr-tools'
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    let sk = generatePrivateKey() // `sk` is a hex string
+    let pk = getPublicKey(sk) // `pk` is a hex string
+    console.log(`You are ${pk}`)
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
